ad74dcf5c9076cfb9f9f017b11089f412d13b075
John Olanipekun <johnolanipekun@Johns-MacBook-Pro.local>
2025-09-01 21:00:38 +0100

    backend changes

--- server/server.mjs
+++ server/server.mjs
@@ -8,6 +8,8 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 import { doc, getDoc } from "firebase/firestore";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -19,13 +21,13 @@ const __dirname = path.dirname(__filename);
 app.use("/icons", express.static(path.join(__dirname, "public/icons")));
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB0zIP3VBk4PHXStN1odtLNe1H2p7xLf_0",
-  authDomain: "inventory-dashboard-5d7d9.firebaseapp.com",
-  projectId: "inventory-dashboard-5d7d9",
-  storageBucket: "inventory-dashboard-5d7d9.firebasestorage.app",
-  messagingSenderId: "1007000310503",
-  appId: "1:1007000310503:web:696c20335f9cb7016bc842",
-  measurementId: "G-ZS3SQ01EYP",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
